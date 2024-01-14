15781c590b74b396bf159d55fac3df81b7fbbe7d
Ana Levit <anaalamed@gmail.com>
2024-01-14 16:42:42 +0200

    initial commit - entry point

---
+++ Assets/MoonActive/Scripts/GameBoardLogic.cs
@@ -0,0 +1,25 @@
+using MoonActive.Scripts;
+
+public class GameBoardLogic
+{
+    public GameBoardLogic(GameView gameView, UserActionEvents userActionEvents)
+    {
+        /*
+         * Constructor
+         */
+    }
+
+    public void Initialize(int columns, int rows)
+    {
+        /*
+         * Initialization logic will be called once per session
+         */
+    }
+    
+    public void DeInitialize()
+    {
+        /*
+         * DeInitialization logic will be called once per session, at disposal
+         */
+    }
+}
