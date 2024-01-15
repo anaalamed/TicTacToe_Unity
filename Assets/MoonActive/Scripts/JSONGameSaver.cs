46c01832ac6148a12c97eab715f299a097dd25f0
Ana Levit <anaalamed@gmail.com>
2024-01-15 13:46:51 +0200

    writing json to file

--- Assets/MoonActive/Scripts/JSONGameSaver.cs
+++ Assets/MoonActive/Scripts/JSONGameSaver.cs
@@ -1,32 +1,33 @@
-﻿using Codice.CM.Client.Differences;
-using MoonActive.Scripts;
+﻿using System;
+using System.IO;
 using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 
 public class JSONGameSaver : GameSaver
 {
-    private string json;
-
+    private readonly string fileName = "gameState.json";
 
     GameState GameSaver.LoadGame()
     {
-        if (json == null) {
-            // TODO throw 
-            //return (null, PlayerType.PlayerX);
-        }
-
-        GameState gameState =JsonConvert.DeserializeObject<GameState>(json);
-        return new GameState
+        try
         {
-            Board = gameState.Board,
-            CurrentPlayer = gameState.CurrentPlayer
-        };
+            Debug.Log("Reading json state game from file: " + fileName);
+            string json = File.ReadAllText(fileName);
+            GameState gameState = JsonConvert.DeserializeObject<GameState>(json);
+            return gameState;
+        }
+        catch (FileNotFoundException)
+        {
+            Debug.LogWarning("The game state was never saved");
+            throw new InvalidOperationException();
+        }
     }
 
     void GameSaver.SaveGame(GameState gameState)
     {
-        json = JsonConvert.SerializeObject(gameState, Formatting.Indented);
-        Debug.Log("json object: , " + json);
+        string json = JsonConvert.SerializeObject(gameState, Formatting.Indented);
+        Debug.Log("Saving json state game to file: " + fileName);
+        File.WriteAllText(fileName, json);
     }
 }
 
